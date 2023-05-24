76d3e6dfa1dbf8ef4e435a7ae5aba3c9eb7b8f80
Sainath Kailas Damre <sainathdamare3011@gmail.com>
2023-05-24 15:33:42 +0530

    binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,3 +1,26 @@
-a=3
-b="shreyas"assg1
-print(a+b, "Hello shreyas")
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1  # Target element not found
+
+# Example usage:
+array = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
+target = 38
+
+index = binary_search(array, target)
+
+if index != -1:
+    print("Target element ",target ,"found at index", index)
+else:
+    print("Target element not found in the array.")
